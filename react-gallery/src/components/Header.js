3d1da42b3edefad661350d3a43d581e80bc8d9fa
Stacy <stacyrays@gmail.com>
2019-04-08 13:20:14 -0700

    Starting to add basic components

---
+++ react-gallery/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Header = ({ players, title }) => {
+  //const { players, title } = props;
+  return (
+    <header>
+      <h1>React Gallery</h1>
+    </header>
+  );
+};
+
+export default Header;
