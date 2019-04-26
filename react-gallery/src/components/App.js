20079d4beafc27cc43d56f811822b35b59ca6754
Stacy <stacyrays@gmail.com>
2019-04-26 10:26:11 -0700

    Updates to the search buttons

--- react-gallery/src/components/App.js
+++ react-gallery/src/components/App.js
@@ -11,11 +11,11 @@ import Gallery from "./Gallery";
 
 class App extends Component {
   state = {
-    search: [
+    images: [
       {
-        term: "dog",
-        imageURL:
-          "https://www.google.com/search?q=dog&oq=dog&aqs=chrome..69i57j69i60l4j0.351j0j7&sourceid=chrome&ie=UTF-8"
+        image:
+          "https://farm66.staticflickr.com/65535/40704563113_a2ccf62825.jpg",
+        id: 1
       }
     ]
   };
@@ -26,6 +26,8 @@ class App extends Component {
     //this.props.history.push(path);
     //console.log("this is the search term " + searchTerm);
   };
+
+  prevPlayerId = 1;
   searchQuery = query => {
     fetch(
       `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
@@ -36,7 +38,12 @@ class App extends Component {
       .then(function(myJson) {
         console.log("json should be happening");
         console.log(JSON.stringify(myJson));
-        console.log(JSON.stringify(myJson.photos.photo[0]));
+        console.log(JSON.stringify(myJson.photos.photo[0].farm));
+        console.log(
+          `https://farm${myJson.photos.photo[0].farm}.staticflickr.com/${
+            myJson.photos.photo[0].server
+          }/${myJson.photos.photo[0].id}_${myJson.photos.photo[0].secret}.jpg`
+        );
       });
   };
   render() {
